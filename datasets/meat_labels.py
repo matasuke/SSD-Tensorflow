812bf8eac84f9314113824bdb47bb1c45afd39bc
matasuke <fmatasuke@gmail.com>
2018-10-16 16:15:30 +0900

    fix data loader for meat_master2018

---
+++ datasets/meat_labels.py
@@ -0,0 +1,15 @@
+MEAT_LABELS = {
+    'none': (0, 'Background'),
+    'raw_beaf': (1, 'beaf'),
+    'half_cooked_beaf': (2, 'beaf'),
+    'cooked_beaf': (3, 'beaf'),
+    'over_cooked_beaf': (4, 'beaf'),
+    'raw_pork': (5, 'pork'),
+    'half_cooked_pork': (6, 'pork'),
+    'cooked_pork': (7, 'pork'),
+    'over_cooked_portk': (8, 'pork'),
+    'raw_chicken': (9, 'chicken'),
+    'half_cooked_chicken': (10, 'chicken'),
+    'cooked_chicken': (11, 'chicken'),
+    'over_cooked_chicken': (12, 'chicken')
+}
